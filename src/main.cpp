693a85dc4e52017778d07108c371ee311f90e1ce
SAM LLOYD <SL295211@falmouth.ac.uk>
2025-09-22 17:25:28 +0100

    added shader class

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,47 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "Engine/Rendering/Shaders/Shader.h"
 
 int main(int argc, char* argv[]) {
+
+	float OpenGLVersion = 3.3;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, (int)OpenGLVersion);
+
+	//do this to get the first digit
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, (int)(OpenGLVersion * 10) % 10);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	GLFWwindow* Window = glfwCreateWindow(800, 600, "SamsEngine", NULL, NULL);
+
+	if (Window == NULL)
+	{
+#if DEBUG
+		std::cout << "Failed to create window" << std::endl;
+#endif
+		glfwTerminate();
+	}
+
+	glfwMakeContextCurrent(Window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+#if DEBUG
+		std::cout << "Failed to initialise glad" << std::endl;
+#endif
+	}
+
+
+	while(true)
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(Window);
+	}
+
 	std::cout << "Hello, world!" << std::endl;
 	return EXIT_SUCCESS;
 }
