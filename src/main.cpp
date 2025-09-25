6d34ed95c5dbb6999b334232080657f4d85e3cb8
SL295211 <SL295211@falmouth.ac.uk>
2025-09-25 18:05:44 +0100

    unit tests working, and project build into library (needs cleaning up and seperating from main project)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <project/main.hpp>
 
 #include <iostream>
 #include <cstdlib>
--- tests/project-unit/project-unit.cpp
+++ tests/project-unit/project-unit.cpp
@@ -1,6 +1,16 @@
+#include <cassert>
 #include <gtest/gtest.h>
+#include "MathCore.h"
+
 
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
+}
+
+TEST(Test1, Math)
+{
+	bool expected = false;
+	bool result = MathCore::IsEven(2);
+	ASSERT_EQ(expected, result);
 }
