8f03da9d141ca73a8c4041eaf5aed8087ee89a75
SAM LLOYD <SL295211@falmouth.ac.uk>
2025-11-28 15:07:47 +0000

    started on smallest circle

--- Engine/src/Engine/Triangulation/DelaunayTriangulation.cpp
+++ Engine/src/Engine/Triangulation/DelaunayTriangulation.cpp
@@ -65,3 +65,87 @@ Triangle DelaunayTriangulation::GetSuperTriangle(const Array<Vector3D>& Vertices
 
 	return { {XRange.X, YRange.X, ZRange.X}, {XRange.X + (diffX / 2), YRange.Y, ZRange.X + (diffZ /2)}, {XRange.Y, YRange.X, ZRange.Y}};
 }
+
+Circle Triangle::GetMinCircleTrivial(Array<Vector3D>& EdgeRPoints)
+{
+	assert(EdgeRPoints.GetSize() <= 3);
+
+	if (EdgeRPoints.IsEmpty()) return { Vector3D::Zero, 0 };
+
+	if (EdgeRPoints.GetSize() == 1) return { EdgeRPoints[0], 0 };
+
+	if (EdgeRPoints.GetSize() == 2) return { EdgeRPoints[0], EdgeRPoints[1] };
+
+	for (size_t i = 0; i < 2; i++)
+	{
+		for (size_t j = i+1; j < 3; j++)
+		{
+			Circle c = { EdgeRPoints[i], EdgeRPoints[j] };
+			if (c.ArePointsInsideCircle(EdgeRPoints)) return c;
+		}
+	}
+	return { EdgeRPoints[0], EdgeRPoints[1], EdgeRPoints[2] };
+}
+
+Circle Triangle::GetSmallestCircle(Array<Vector3D>& Points, Array<Vector3D>& EdgeRPoints, )
+{
+	if (Points.IsEmpty() || EdgeRPoints.GetSize() == 3) return GetMinCircleTrivial(EdgeRPoints);
+
+
+}
+
+bool Triangle::IsPointInCircumference(const Vector3D& Point)
+{
+	Array<Vector3D> Points = { point1, point2, point3 };
+	Array<Vector3D> EdgeRPoints;
+
+
+
+
+		float circumference =
+
+	return false;
+}
+
+Circle::Circle(const Vector3D& P1, const Vector3D& P2)
+{
+	Pos = (P1 + P2) / 2;
+
+	Radius = ((P1 - P2) / 2).GetLength();
+
+}
+
+Circle::Circle(const Vector3D& P1, const Vector3D& P2, const Vector3D& P3)
+{
+	Pos = GetCircleCenter(P2 - P1, P3 - P1);
+
+	Radius = (Pos - P1).GetLength();
+}
+
+Vector3D Circle::GetCircleCenter(const Vector3D& AToB, const Vector3D& AToC)
+{
+	const double b = AToB.GetSquaredLength();
+	const double c = AToC.GetSquaredLength();
+
+	const Vector3D d = Vector3D::Cross(AToB, AToC);
+
+	return (Vector3D::Dot(AToB, AToB) * Vector3D::Cross(AToC, d) + Vector3D::Dot(AToC, AToC) * Vector3D::Cross(d, AToB)) / (2 * Vector3D::Dot(d, d));
+}
+
+bool Circle::ArePointsInsideCircle(const Array<Vector3D>& Points)
+{
+	for (const auto& p : Points)
+	{
+		if (IsPointInsideCircle(p)) continue;
+		return false;
+	}
+
+	return true;
+}
+
+bool Circle::IsPointInsideCircle(const Vector3D& Point)
+{
+	if ((Point - Pos).GetSquaredLength() <= (Radius * Radius)) return true;
+
+	return false;
+}
--- Engine/src/Engine/Triangulation/DelaunayTriangulation.h
+++ Engine/src/Engine/Triangulation/DelaunayTriangulation.h
@@ -5,23 +5,36 @@
 #include "SubsystemInitialiser.h"
 
 
+struct Circle
+{
+	Vector3D Pos;
+	float Radius;
+
+	Circle(const Vector3D& InPos, const float& InRadius) : Pos(InPos), Radius(InRadius) {};
+
+	Circle(const Vector3D& P1, const Vector3D& P2);
+
+	Circle(const Vector3D& P1, const Vector3D& P2, const Vector3D& P3);
+
+	Vector3D GetCircleCenter(const Vector3D& AToB, const Vector3D& AToC);
+
+	bool ArePointsInsideCircle(const Array<Vector3D>& Points);
+
+	bool IsPointInsideCircle(const Vector3D& Point);
+
+};
+
 struct Triangle
 {
 	Vector3D point1;
 	Vector3D point2;
 	Vector3D point3;
 
-	bool IsPointInCircumference(const Vector3D& Point)
-	{
-		Vector2D XRange;
-		Vector2D YRange;
-		Vector2D ZRange;
+	Circle GetMinCircleTrivial(Array<Vector3D>& EdgeRPoints);
 
-		if (point1 > )
+	Circle GetSmallestCircle(Array<Vector3D>& Points, Array<Vector3D>& EdgeRPoints);
 
-
-		float circumference = 
-	}
+	bool IsPointInCircumference(const Vector3D& Point);
 };
 
 class DelaunayTriangulation
