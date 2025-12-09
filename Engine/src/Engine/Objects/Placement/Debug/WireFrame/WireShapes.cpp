81a8fa611c00fa3c8bbbb213a03fdb7c2eb91a63
SL295211 <SL295211@falmouth.ac.uk>
2025-12-09 19:57:30 +0000

    didnt get coomited

--- Engine/src/Engine/Objects/Placement/Debug/WireFrame/WireShapes.cpp
+++ Engine/src/Engine/Objects/Placement/Debug/WireFrame/WireShapes.cpp
@@ -70,10 +70,10 @@ void WireObject::Initialise()
 	//glBindVertexArray(VAO);
 	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.GetSize(), Vertices.GetFirstRef(), GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.GetSize(), Vertices.GetFirstPtr(), GL_STATIC_DRAW);
 
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.GetSize(), Indices.GetFirstRef(), GL_STATIC_DRAW);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.GetSize(), Indices.GetFirstPtr(), GL_STATIC_DRAW);
 
 	//glEnableVertexAttribArray(0);
 	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), static_cast<void*>(0));
